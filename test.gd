e0ccd59f8969e1202eb193d3b67293f649449fa2
allefrodrigo <dev.allefschmidt@gmail.com>
2024-06-15 12:50:45 -0300

    icon moves according to the sound played

---
+++ test.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var action = 0
+
+func _ready():
+	# Conecte o sinal midi_event ao método usando Callable
+	$MIDIPlayer.connect("midi_event", Callable(self, "_on_midi_player_midi_event"))
+
+func _on_midi_player_midi_event(channel, event):
+	if event.type == 144:  # Note on event
+		print("Note on event received: ", event.note)
+		if action == 0:
+			var tween = create_tween()
+			tween.tween_property($Icon, "global_position", Vector2(600,323),0.2)
+			action = 1
+		elif action == 1:
+			var tween = create_tween()
+			tween.tween_property($Icon, "global_position", Vector2(217,323),0.2)
+
+			action = 0
