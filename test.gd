4f26a59bba4cfe0741cba923d329f0e2d9f7f3d5
allefrodrigo <dev.allefschmidt@gmail.com>
2024-06-18 17:01:58 -0300

    sound added

--- test.gd
+++ test.gd
@@ -4,8 +4,11 @@ var action = 0
 
 func _ready():
 	# Conecte o sinal midi_event ao método usando Callable
+	
 	$MIDIPlayer.connect("midi_event", Callable(self, "_on_midi_player_midi_event"))
-
+	if $ADSR.has_method("play"):
+		$ADSR.play()
+		
 func _on_midi_player_midi_event(channel, event):
 	if event.type == 144:  # Note on event
 		print("Note on event received: ", event.note)
